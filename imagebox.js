55115e18e3be686d0501651e62654ef6e292fb22
Gun.io Whitespace Robot <contact@gun.io>
2011-12-17 10:00:33 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- imagebox.js
+++ imagebox.js
@@ -3,33 +3,33 @@
  * simple and fancy lightbox alternative
  * By Konstantin Dankov (http://dankov.name)
  * Examples and documentation at: https://github.com/kdankov/imagebox
- * 
+ *
  * Version: 2.0 Beta 1
  * Requires: jQuery v1.3+
- * 
+ *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  */
 
 function ib_init(){
-	
+
 	// --------------------------------------------------------
 	// Configuration area ---- Make changes here when needed
 	// --------------------------------------------------------
-	
+
 		var ib_information = true;
 		var ib_external_padding = 100;
 		var ib_internal_padding = 5;
 		var ib_animate_time = 300;
-		
+
 		// Debug information when things get bad
 		var ib_debug = true; // Show information
-		
+
 	// --------------------------------------------------------
 	// Configuration area ends here
 	// --------------------------------------------------------
-	
+
 	// Checking for console support
 	if ( typeof console == "object" ) {
 		var support_for_console = true;
@@ -38,7 +38,7 @@ function ib_init(){
 	{
 		var support_for_console = false;
 	}
-	
+
 	// Appending html structure to the page
 	$('body').append('<div id="ib_overlay"></div>');
 	$('body').append('<div id="ib_box"><div id="ib_inner"><img id="ib_image" src="" alt="" title="" /><div id="ib_info"><div id="ib_data"><h4></h4><p></p></div></div><div id="ib_close">X Close</div></div></div>');
@@ -52,19 +52,19 @@ function ib_init(){
 	var ib_info_panel_data 		= ib_info_panel.find('#ib_data');
 	var ib_info_panel_header 	= ib_box.find('h4');
 	var ib_info_panel_content	= ib_info_panel.find('p');
-	
+
 	// OnClick event - starts the thing
 	$('.imagebox').click(function(){
-		
+
 		// --------------------------------------------------------
 		// Do not touch the code below if you don't know what are you doing
 		// --------------------------------------------------------
-		
+
 		// Get the window size. This is part of the resizing functionality it resizes bigger pictures to fit inside your window.
 		var ib_pagesize = ib_getPageSize();
 		var ib_pagesize_width = ib_pagesize[0];
 		var ib_pagesize_height = ib_pagesize[1];
-		
+
 		// Get box padding from CSS
 		var ib_padding_temp = ib_box.css("padding-top");
 		var ib_numbers = new RegExp("\\d*")
@@ -76,7 +76,7 @@ function ib_init(){
 		var body_scroll				= $.browser.mozilla ? $("html").scrollTop() : $("body").scrollTop(); // For some strange reason the scrolling is handled by the HTML tag in Firefox
 		var ib_link_image_width		= $(this).width() - ib_padding*2;
 		var ib_link_image_height	= $(this).height() - ib_padding*2;
-		
+
 
 		// Hide the ImageBox window and its elements
 		ib_box.hide();
@@ -84,19 +84,19 @@ function ib_init(){
 
 		// Sets the dimensions and the position of the imagebox div just on top of the link that is triggering the event.
 		ib_box.css({ width: ib_link_image_width, height: ib_link_image_height, position: "absolute", top: ib_link_offset.top, left: ib_link_offset.left }); // Return the box to the center of the window to be ready for the next image.
-		
+
 		// Show the overlay div - Curtains up
 		ib_overlay.show();
 
 		// Get the closing event attached
-		$( '#ib_overlay, #ib_close' ).click(function(){ 
+		$( '#ib_overlay, #ib_close' ).click(function(){
 			ib_box.fadeOut( ib_animate_time, function (){
 				ib_info_panel.hide();
 				ib_overlay.fadeOut( ib_animate_time );
 				ib_pic.attr({ width: "50px", height: "50px" });
 			});
 		});
-		
+
 		// Create a new Image object and assign URI to it. Takes the href from the link and loads it as sorce for the image.
 		ib_preload_image = new Image();
 		ib_preload_image.src = ib_link.attr('href');
@@ -104,17 +104,17 @@ function ib_init(){
 		// Checks if the link is actually an Image
 		var ib_urlString = /\.jpg$|\.jpeg$|\.png$|\.gif$|\.bmp$/;
 	    var ib_urlType = ib_preload_image.src.toLowerCase().match(ib_urlString);
-		
+
 		// Launch this when the new image is loaded.
 		function ib_image_loaded()
 		{
-			
+
 			// Get the new image dimensions and prepare for sime calculation
 			var pwidth = ib_preload_image.width;
 			var pheight = ib_preload_image.height;
 			var pwidth2 = 0;
 			var pheight2 = 0;
-			
+
 			if ( ib_debug && support_for_console )
 			{
 				console.info( "----------- Window dimension -----------" );
@@ -150,14 +150,14 @@ function ib_init(){
 					console.info( "--- Width: " + pwidth + "px, Height: " + pheight + "px" );
 				}
 			}
-			
+
 			// Calculate the box size
 			var boxwidth = pwidth;
 			var boxheight = pheight;
 
 			var box_pos_left = (ib_pagesize_width - boxwidth) / 2;
 			var box_pos_top = ((ib_pagesize_height - boxheight) / 2) + body_scroll;
-			
+
 
 			if ( ib_debug && support_for_console )
 			{
@@ -169,15 +169,15 @@ function ib_init(){
 				console.info( "Top:" + box_pos_top + ", Left: " + box_pos_left );
 			}
 
-			
+
 			// Calculate the margins needed for positioning the box
 			var margint = boxheight / 2;
 			var marginl = boxwidth / 2;
-			
+
 			// Get information from the img element in the link
 			var ib_pic_title = ib_link.find("img").attr("title");
 			var ib_pic_alt = ib_link.find("img").attr("alt");
-			
+
 			if ( ib_debug && support_for_console )
 			{
 				console.info( "----------- Picture text information -----------" );
@@ -185,7 +185,7 @@ function ib_init(){
 				console.info( "Picture description: " + ib_pic_alt );
 				console.info( "************ End of report ************ ");
 			}
-			
+
 			// Set image to the right size
 			ib_pic.attr({ width: pwidth, height: pheight, src: ib_preload_image.src });
 
@@ -197,17 +197,17 @@ function ib_init(){
 			ib_box
 				.show()
 				.animate({ width: boxwidth, height: boxheight, position: "fixed", top: box_pos_top, left: box_pos_left }, ib_animate_time, function (){
-					
+
 					ib_pic.fadeIn( ib_animate_time );
-				
+
 					$(this).hover(
 						function(){	ib_info_panel.fadeIn( ib_animate_time );  },
 						function(){	ib_info_panel.fadeOut( ib_animate_time ); }
 					);
 			});
-			
+
 		}
-	    
+
 		if(ib_urlType == '.jpg' || ib_urlType == '.jpeg' || ib_urlType == '.png' || ib_urlType == '.gif' || ib_urlType == '.bmp')
 		{
 			if ( ib_debug && support_for_console ){
@@ -219,15 +219,15 @@ function ib_init(){
 			if( ib_preload_image.complete ){
 				ib_image_loaded();
 			}else {
-				ib_preload_image.onload = function(){ 
+				ib_preload_image.onload = function(){
 					ib_image_loaded();
 				}
 			}
-			
+
 		}else {
 			window.open( ib_urlString );
 		}
-		
+
 		return false;
 	});
 
@@ -239,7 +239,7 @@ function ib_getPageSize()
 	var w = window.innerWidth || self.innerWidth || (de&&de.clientWidth) || document.body.clientWidth;
 	var h = window.innerHeight || self.innerHeight || (de&&de.clientHeight) || document.body.clientHeight
 
-	arrayPageSize = new Array(w,h) 
+	arrayPageSize = new Array(w,h)
 
 	return arrayPageSize;
 }
